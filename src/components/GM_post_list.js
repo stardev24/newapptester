18f09e2d2741aea4ab746628246093c19e5ee10f
stardev24 <stardev24@gmail.com>
2018-06-12 20:10:51 +0530

    Added changes

--- src/components/GM_post_list.js
+++ src/components/GM_post_list.js
@@ -4,7 +4,7 @@ import { FetchPostsData,baseURL} from "../action/userpostActions"
 import {Button} from 'react-materialize'
 
 
-class PostDetailsShortFormat extends Component {
+class GMListView extends Component {
 	
 
 
@@ -28,21 +28,19 @@ class PostDetailsShortFormat extends Component {
 			   <div>
 				      	<div className="row">
 				      	<div className="col s12 border">
-					        <span className="col s4">Subject</span>
-					        <span className="col s3">Status</span>
-					        <span className="col s3">Creation Date</span>
-					        <span className="col s2">Details</span>
+					        <span className="col s4">Author</span>
+					        <span className="col s4">Status</span>
+					        <span className="col s4">Details</span>
 				        </div>
 				        </div>
-				        	
+
 
 				      {this.state && this.state.postsData && this.state.postsData.map(item =>
 				      	<div className="row">
 				      	<div className="col s12 border">
-					        <span className="col s4">{item.subject}</span>
-					        <span className="col s3">{item.status}</span>
-					        <span className="col s3">{item.createdDate}</span>
-					        <span className="col s2"><Button className="test">More..</Button></span>
+					        <span className="col s4">{item.author}</span>
+					        <span className="col s4">{item.status}</span>
+					        <span className="col s4"><Button className="test">Show More</Button></span>
 				        </div>
 				        </div>
 				      )}
@@ -68,4 +66,4 @@ const mapStateToProps = (state) => {
         };
     };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostDetailsShortFormat);
+export default connect(mapStateToProps, mapDispatchToProps)(GMListView);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store/store'
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {SuggestionsList} from "./components/suggestions_listview"
+import {LandingPage} from "./components/landingpage"
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+	  <Router>
+	    	<App />
+	  </Router>
   </Provider>,
 	document.getElementById('root'));
 registerServiceWorker();
